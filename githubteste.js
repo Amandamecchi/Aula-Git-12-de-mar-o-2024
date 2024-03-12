7f83c318a7904042f589dec699cb40ee11f7ae5d
Amanda Gomes Mechi <amandamechi327@gmail>
2024-03-12 09:41:18 -0300

    alterei a váriave número e a variável limite tabuada

--- githubteste.js
+++ githubteste.js
@@ -1,5 +1,5 @@
-let numero = 5;
-let limiteTabuada = 10;
+let numero = 6;
+let limiteTabuada = 11;
 let = numero * limiteTabuada;
 for (let i = 1; i <= limiteTabuada; i++) {
     console.log(numero ,"x" , i , "= " , numero * i);
